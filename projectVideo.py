f34e5df6159cedd7c3763192f361680deb0843d0
James Deal <j891319@protonmail.com>
2022-09-26 09:06:23 +0800

    finished proof of concept code

--- projectVideo.py
+++ projectVideo.py
@@ -10,7 +10,40 @@ import sys
 import cv2
 import numpy as np
 
-def findBorder(webFrame,target,w,h, orb, keyPointsWeb,keyPoints,successfullMatches):
+def overlay(webFrame, sourceFrame, target, homographyMatrix, destinationPoints,w,h):
+    # we take the warped source frame and the webcam frame and overlay the two
+    # this is done using masks of black and white and using AND/XOR operators 
+    # this as as we can delete the sections over the detected target with the AND
+    # as if one is a white (1) mask and one is black (0) they will result in a 0 result
+    # and then use the XOR to overlay the source onto that area as it will use the positive
+    # source bits over the negative black area where the target was.
+
+    # warp the source frame by the homography we calcuated 
+    warpedSource = cv2.warpPerspective(sourceFrame, homographyMatrix, (w,h))
+    # create a blank mask of 0s in the dimensions of the webcam frame
+    mask2 = np.zeros(webFrame.shape, dtype=np.uint8)
+    # create a white mask with a black box where the target was detected
+    cv2.fillConvexPoly(mask2, destinationPoints, (255,255,255))
+
+    
+    mask2 = cv2.bitwise_not(mask2)
+
+    print(webFrame.shape)
+    print(mask2.shape)
+    print(destinationPoints.shape)
+
+    masked_image2 = cv2.bitwise_and(webFrame, mask2)
+    cv2.imshow("masked image", masked_image2)
+
+    #Using Bitwise or to merge the two images
+    final = cv2.bitwise_or(warpedSource, masked_image2)
+    cv2.imshow("mask", mask2)
+    cv2.imshow("web", webFrame)
+    cv2.imshow("source", sourceFrame)
+    cv2.imshow("overlay", final)
+
+
+def findBorder(webFrame,target, sourceFrame,w,h, orb, keyPointsWeb,keyPoints,successfullMatches):
     print("finding border")
     # we need to reformat the data in the successful matches to allow us to put it into cv2.findHomography 
     # this is done using numpy's .reshape and a numpy array
@@ -27,19 +60,20 @@ def findBorder(webFrame,target,w,h, orb, keyPointsWeb,keyPoints,successfullMatch
     
     # we now create the base frame of the source which is just the dimenions of the image
     # we calculated this previously so I'll just pass in the variables.
-    sourcePoints = np.float32([[0,0],[0,h],[w,h],[w,0]]).reshape(-1,1,2)
+    sourcePoints = np.float32([[0,0],[w1,0],[w1,h1],[0,h1]]).reshape(-1,1,2)
 
     # now we warp those border points based on where cv2 thinks it's found the same points through the webcam
     # this is done using the homography matrix
     destinationPoints = cv2.perspectiveTransform(sourcePoints,homographyMatrix)
     # we now add these border lines of the target image to the webcam frame
-    cv2.polylines(webFrame,[np.int32(destinationPoints)],True,(255,255,255),3)
-
+    #cv2.polylines(webFrame,[np.int32(destinationPoints)],True,(255,255,255),3)
 
     # this a built in cv2 function which shows a drawn line of matched features parralel to each other
-    features = cv2.drawMatches(target,keyPoints,webFrame,keyPointsWeb,successfullMatches,None,flags=2)
-    print("showing frame")
-    cv2.imshow("features", features)
+    # features = cv2.drawMatches(target,keyPoints,webFrame,keyPointsWeb,successfullMatches,None,flags=2)
+    # print("showing frame")
+    # cv2.imshow("features", features)
+
+    overlay(webFrame, sourceFrame, target, homographyMatrix, np.int32(destinationPoints), w, h)
 
 
 def recognizeCover(webFrame, descriptors, descriptors2, descriptors3, descriptorsWeb):
@@ -71,20 +105,22 @@ def recognizeCover(webFrame, descriptors, descriptors2, descriptors3, descriptor
             successfullMatches3.append(targetMatch)
     # if there are more than 20 matches we assume it's a positive match 
     # and return which magazine it is using an integer code alongside the keypoints matched in a tuple
-    if len(successfullMatches) > 20:
+    if len(successfullMatches) > 17:
         return successfullMatches, 1
-    elif len(successfullMatches2) > 20:
+    elif len(successfullMatches2) > 17:
         return successfullMatches2, 2
-    elif len(successfullMatches3) > 20:
+    elif len(successfullMatches3) > 17:
         return successfullMatches3, 3
     else:
         return None, None
 
 
-def webcamRead(feed,target, target2, target3, w,h, orb, keyPoints, keyPoints2, keyPoints3, descriptors, descriptors2, descriptors3):
+def webcamRead(feed, source,target, target2, target3, orb, keyPoints, keyPoints2, keyPoints3, descriptors, descriptors2, descriptors3):
+    global h1,w1,c1
     frameLoaded, webFrame = feed.read()
+    sourceLoaded, sourceFrame = source.read()
     print("reading webcam")
-    while frameLoaded:
+    while frameLoaded and sourceLoaded:
 
         # read the keypoints in the orb of the webcam frames
         # we can use these to compare to the target keypoints
@@ -93,19 +129,31 @@ def webcamRead(feed,target, target2, target3, w,h, orb, keyPoints, keyPoints2, k
         # find which magazine is in the webcam and store the matching points (later used for creating a homography matrix)
         matches, targetNum = recognizeCover(webFrame, descriptors, descriptors2, descriptors3, descriptorsWeb)
 
+        h, w, c = webFrame.shape
+        h1, w1, c1 = target.shape
+
+        # resizing all targets and source to be the same
+        # .shape on a loaded image returns a tuple of height, width, and channels
+        target2 = cv2.resize(target2,(w1,h1))
+        target3 = cv2.resize(target3,(w1,h1))
+        sourceFrame = cv2.resize(sourceFrame,(w1,h1))
+        # wait for keypress and if keypress is q or Q, break loop
+
         # find the border of the target image found through the webcam based on which target cover it is
         if targetNum != None:
             if targetNum == 1:
-                findBorder(webFrame,target, w,h, orb, keyPointsWeb,keyPoints,matches)
+                findBorder(webFrame,target,sourceFrame, w,h, orb, keyPointsWeb,keyPoints,matches)
             elif targetNum == 2:
-                findBorder(webFrame, target2, w,h, orb, keyPointsWeb,keyPoints2,matches)
+                findBorder(webFrame, target2,sourceFrame, w,h, orb, keyPointsWeb,keyPoints2,matches)
             elif targetNum == 3:
-                findBorder(webFrame, target3, w,h, orb, keyPointsWeb,keyPoints3,matches)
+                findBorder(webFrame, target3,sourceFrame, w,h, orb, keyPointsWeb,keyPoints3,matches)
+
+
 
         # load the next frame and update boolean
         frameLoaded, webFrame = feed.read()
+        sourceLoaded, sourceFrame = source.read()
 
-        # wait for keypress and if keypress is q or Q, break loop
         buttonPress = cv2.waitKey(1)
         if buttonPress==81 or buttonPress==113:
             sys.exit()
@@ -122,14 +170,8 @@ def main():
     target3 = cv2.imread("target3.jpg")
     # load source video to project onto the target
     source = cv2.VideoCapture("source.mp4")
-    sourceRead, frame = source.read()
 
-    # resizing all targets and source to be the same
-    # .shape on a loaded image returns a tuple of height, width, and channels
-    h, w, c = target.shape
-    frame = cv2.resize(frame,(w,h))
-    target2 = cv2.resize(target2,(w,h))
-    target3 = cv2.resize(target3,(w,h))
+
 
     # using cv2 ORB which is a feature which creates image detector keypoints
     # nfeatures specifices the number of features to use as matching points
@@ -142,7 +184,7 @@ def main():
     keyPoints2, descriptors2 = orb.detectAndCompute(target2,None)
     keyPoints3, descriptors3 = orb.detectAndCompute(target3,None)
 
-    webcamRead(feed, target, target2, target3, w,h, orb,keyPoints, keyPoints2, keyPoints3, descriptors, descriptors2, descriptors3)
+    webcamRead(feed, source, target, target2, target3, orb,keyPoints, keyPoints2, keyPoints3, descriptors, descriptors2, descriptors3)
 
 
 if __name__ == "__main__":
