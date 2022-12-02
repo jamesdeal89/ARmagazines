f2e6388c49bfb1a48bded4af8426828b4def872d
James Deal <j891319@protonmail.com>
2022-12-02 09:38:30 +0800

    finished warp class using imported algoritm

--- OOP/warp.py
+++ OOP/warp.py
@@ -2,11 +2,43 @@
 import cv2
 class Warp():
     def __init__(self, sourceFrame, homographyMatrix, dimensions):
-        self._warpedImg = None
-        self._homographyMatrix = homographyMatrix
-        self._sourceFrame = sourceFrame
-        self._dimensions = dimensions
+        self.warpedImg = None
+        self.homographyMatrix = homographyMatrix
+        self.sourceFrame = sourceFrame
+        self.dimensions = dimensions
     
+    @property
+    def warpedImg(self):
+        return self._warpedImg
+
+    @property
+    def homographyMatrix(self):
+        return self._homographyMatrix
+
+    @property
+    def sourceFrame(self):
+        return self._sourceFrame
+
+    @property
+    def dimenions(self):
+        return self._dimensions
+
+    @warpedImg.setter
+    def warpedImg(self,img):
+        self._warpedImg = img
+
+    @homographyMatrix.setter
+    def homographyMatrix(self,matrix):
+        self._homographyMatrix = matrix
+
+    @sourceFrame.setter
+    def sourceFrame(self,frame):
+        self._sourceFrame = frame
+
+    @dimensions.setter
+    def dimesnions(self,list):
+        self._dimensions = list
+
     def warp(self):
         """
         Takes an image frame in the form of an OpenCV object.
