f242cba9f4cea93a4b1229733909290b03f2d3d2
user <user@localhost.localdomain>
2023-02-05 20:26:07 +0800

    completed searching algorithm - passes tests

--- OOP/search.py
+++ OOP/search.py
@@ -1,31 +1,60 @@
 """ This is a class for searching a filesystem for a filename. It allows more forgiving user input than default python"
 """
 class Search():
-    def __init__(self, filename):
+    def __init__(self, filename, unordList):
         self._filename = filename
+        self._unordList = unordList
         self._sortedDir = None
 
-    def sort(self,unordList):
+    def sort(self):
         """
         this method uses insetion sort to sort the files in the directory currently searching
         necesary to allow for searching later with binary search
         in insertion sort we take values in an unsorted list and continously insert them into their appropriate position in a sorted list
         """
+        # insertion sort --> O(n^2) time complexity 
         # for each item in the unordered list
-        for i in range(1, len(unordList)):
+        for i in range(1, len(self._unordList)):
             position = i 
             # while we arent at the start index and the current element is less than the previous element,
-            while position > 0 and unordList[position-1] > unordList[position]:
+            while position > 0 and self._unordList[position-1] > self._unordList[position]:
                 # swap the two elements if they are not in the correct order
-                unordList[position], unordList[position-1] = unordList[position-1], unordList[position]
+                self._unordList[position], self._unordList[position-1] = self._unordList[position-1], self._unordList[position]
                 # change the position value to be the previous element
                 position -= 1
-        # once all loops are broken, return the ordered list
-        return unordList
+        # once all loops are broken, set the sortedDir attribute to the sorted list
+        self._sortedDir = self._unordList
 
-    def search(self,unordList):
+        # line below only used if testing the function with pytest
+        return self._sortedDir
+
+    def binarySearch(self,sortedDir,target,start,end):
+        if start > end:
+            # file not found, return False
+            return False
+        else:
+            # calculate mid-point
+            mid = (start + end) //2
+            # check if mid-point is the file 
+            if target == sortedDir[mid]:
+                # file exists, return True
+                return True
+            # otherwise check if file would be above or below the mid-poiny
+            elif target > sortedDir[mid]:
+                # recur and exclude the first half of the list
+                return self.binarySearch(sortedDir,target,mid+1,end)
+            elif target < sortedDir[mid]:
+                # recur and exclude the second half of the list
+                return self.binarySearch(sortedDir,target,start,mid-1)
+
+
+    def search(self):
         """
-        this uses binary search on an ordered list to check the filename is in the directory.
-        returns a boolean value and takes an unordered list as a parameter (uses insertion sort first).
+        this uses binary search on an ordered list to check if the filename is in the directory.
+        returns a boolean value and takes no parameters. MUST call sort() method first.
         """
-        self._sortedDir = self.sort(unordList)
+        if self._sortedDir != None:
+            # completes a binary search algorithm --> O(log n) complexity 
+            return self.binarySearch(self._sortedDir,self._filename,0,len(self._sortedDir)-1)
+        else:
+            sys.exit("ERROR - call sort() method before search() method")
--- OOP/test_search.py
+++ OOP/test_search.py
@@ -1,14 +1,28 @@
 # test file for search.py
 from search import Search
 
-search = Search(None)
-
 def test_sort():
-    assert search.sort([3,2,1,6]) == [1,2,3,6]
-    assert search.sort([1,2,3,4]) == [1,2,3,4]
-    assert search.sort([1,1,1,1,2,1,1,1,1,1]) == [1,1,1,1,1,1,1,1,1,2]
-    assert search.sort(["a","b","c"]) == ["a","b","c"]
-    assert search.sort(["c","b","a"]) == ["a","b","c"]
+    search = Search(None,[3,2,1,6])
+    assert search.sort() == [1,2,3,6]
+    search = Search(None,[1,2,3,4])
+    assert search.sort() == [1,2,3,4]
+    search = Search(None,[1,1,1,1,2,1,1,1,1,1])
+    assert search.sort() == [1,1,1,1,1,1,1,1,1,2]
+    search = Search(None,["a","b","c"])
+    assert search.sort() == ["a","b","c"]
+    search = Search(None,["c","b","a"])
+    assert search.sort() == ["a","b","c"]
 
 def test_search():
-    pass
+    search = Search("a-file", ["c-file","b-file","a-file"])
+    search.sort()
+    assert search.search() == True
+    search = Search("a-file", ["a-file","b-file","c-file"])
+    search.sort()
+    assert search.search() == True
+    search = Search("a-file", ["c-file","a-file","b-file"])
+    search.sort()
+    assert search.search() == True
+    search = Search("a-file", ["b-file","c-file","d-file"])
+    search.sort()
+    assert search.search() == False
