d3977fb397f815953d6a192db26538d0a011e98e
James Deal <j891319@protonmail.com>
2023-02-06 20:23:59 +0800

    added bitwise module

---
+++ OOP/bitwise.py
@@ -0,0 +1,17 @@
+"""This class is for the bitwise operations which have to be performed on the webcam and source video to create a projection effect."""
+class Bitwise():
+    def __init__(self):
+        pass
+
+    
+    def and():
+        # perform a bitwise AND between the two images
+        pass
+
+    def or():
+        # perform a bitwise OR between the two images
+        pass
+
+    def not():
+        # perfrom a bitwise NOT between the two images
+        pass
